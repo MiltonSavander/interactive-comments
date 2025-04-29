8ec4c53c696f2fb2cf3dde8cf79819285ff9308a
Milton <milton.savander@gmail.com>
2025-04-29 21:28:41 +0200

    init - first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./globals.css";
+
+function App() {
+  return (
+    <>
+      <div className="h text-green-500 bg-sky-900">hello world</div>
+    </>
+  );
+}
+
+export default App;
